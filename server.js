15906d1a6ea4838b33c28b15b3bc2a2f8dbe8714
aniruth-gundawar <140300097+aniruth-gundawar@users.noreply.github.com>
2023-07-25 18:50:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import path from "path";
+import {fileURLToPath} from 'url';
 dotenv.config();
 
 connectDB();
+
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 const app = express();
 
